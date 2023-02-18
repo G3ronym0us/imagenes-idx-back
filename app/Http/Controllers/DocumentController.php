5c579d895bafa68703d6ef9a0053f2fb48261b6a
Diohandres Hernandez <diohandres1703@hotmail.com>
2023-02-18 10:50:03 -0400

    set administrator

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -12,7 +12,7 @@ use App\Models\Archivo;
 class DocumentController extends Controller
 {
     /**
-     * Display a liting of the resource.
+     * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
